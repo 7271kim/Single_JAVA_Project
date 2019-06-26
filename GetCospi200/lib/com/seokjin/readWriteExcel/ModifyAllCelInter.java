13d38bce9ec1676ab863ad0350da76bf94d32657
김석진 <7271kim@naver.com>
2019-06-26 16:58:29 +0900

    [2019.06.26] - JAVA 프로젝트 공부하기 위해 전체 정리

--- GetCospi200/lib/com/seokjin/readWriteExcel/ModifyAllCelInter.java
+++
@@ -1,7 +0,0 @@
-package com.seokjin.readWriteExcel;
-
-import org.apache.poi.xssf.usermodel.XSSFCell;
-
-public interface ModifyAllCelInter {
-    void modifyAllCellsWithOutMethod(XSSFCell cell);
-}
--- GetCospi200/src/com/seokjin/calculation/CalculationInter.java
+++
@@ -1,8 +0,0 @@
-package com.seokjin.calculation;
-
-import java.util.ArrayList;
-
-public interface CalculationInter {
-    double mean ( ArrayList<Double> arr); // Double 리스트 형식으로 arr 집어넣으면 mean이 나옴
-    double deviation ( ArrayList<Double> arr); // Double 리스트 형식으로 arr 집어넣으면 표준편차가 나옴
-}
