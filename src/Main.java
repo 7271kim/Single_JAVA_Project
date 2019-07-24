1522e4b2ae1a6dd249fe6a995b55ba47d3b5a294
김석진 <7271kim@naver.com>
2019-07-24 19:00:55 +0900

    [2019.07.24] - 알고리즘 중

--- src/Main.java
+++ src/Main.java
@@ -1,65 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 public class Main{
     public static void main(String args[]) throws IOException{
         /*
-         * https://www.acmicpc.net/problem/5639
+         * https://www.acmicpc.net/problem/7453
          * 알고리즘 문제
-         * 마지막 문자열까지 받기
-         * 이진트리 한번도 안해본 것.
-         * 트리 데이터를 저장하는 것.
          */
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String inputData = br.readLine();
-        BinaryTree result = new BinaryTree(Integer.parseInt(inputData));
-        while ( (inputData = br.readLine()) != null && inputData.length() != 0) {    //EOF까지 입력받음
-            result.addTree(result, Integer.parseInt(inputData));
+        int totalRow =  Integer.parseInt(br.readLine());
+        ArrayList<Integer> firstIndex = new ArrayList<Integer>();
+        ArrayList<Integer> secondIndex = new ArrayList<Integer>();
+        ArrayList<Integer> thirdIndex = new ArrayList<Integer>();
+        ArrayList<Integer> fourIndex = new ArrayList<Integer>();
+        for( int index = 0; index < totalRow; index++ ) {
+        	String [] inputData  = br.readLine().split(" ");
+        	 firstIndex.add(Integer.parseInt(inputData[0]));
+             secondIndex.add(Integer.parseInt(inputData[1]));
+             thirdIndex.add(Integer.parseInt(inputData[2]));
+             fourIndex.add(Integer.parseInt(inputData[3]));
         }
+        int result=0;
+        for(int first : firstIndex ) {
+        	for(int second : secondIndex) {
+        		for(int third : thirdIndex) {
+        			for( int four : fourIndex ) {
+        				if(first+second+third+four == 0) result++;
+        			}
+        		}
+        	}
+        }
+        System.out.println(result);
         
-        printPostOrder(result);
     }
-    
-    public static  void printPostOrder( BinaryTree inputData ) {
-        if( inputData.left != null ) printPostOrder( inputData.left);
-        if( inputData.right != null ) printPostOrder(inputData.right);
-        System.out.println(inputData.node);
-    }
-}
-
-class BinaryTree {
-    int node;
-    BinaryTree left;
-    BinaryTree right;
-    
-    public BinaryTree( int node ) {
-        this.node   = node;
-        this.left  = null;
-        this.right  = null;
-    }
-    
-    /*
-     CORE : Tree 또한 저장된 데이터 형식이다. >>  BinaryTree result = new BinaryTree(Integer.parseInt(inputData)); 결과 하나하나 나무 데이터가 만들어지는 것.
-     Defaul : 노드가 1개일 경우 ( 50 30 60 >> 정상 노출 ) : 핵심로직 >> 여러개일때 동기화작전 >> 회귀 >> 왼쪽 오른쪽 로직이 반복된다. >> 반복하는 것을 종료 조건과 더불어 회귀한다. >> 핵심은 . 달아 놓을것이 무엇인가. 왼쪽 오른쪽은 트리데이터가 달릴수 있다.
-     public BinaryTree addTree ( BinaryTree orignalTree, int value ) {
-        BinaryTree tempBinaryTree = new BinaryTree(value);
-        if( value < orignalTree.node ) {
-            orignalTree.lefet = tempBinaryTree;
-        } else if( value > orignalTree.node ) {
-            orignalTree.right = tempBinaryTree;
-        }
-        return orignalTree;
-    }*/
-    
-    // 반복해야 하는 것을 종료 조건과 더불어 회귀시긴다.
-    public BinaryTree addTree ( BinaryTree orignalTree, int value ) {
-        if( orignalTree == null ) return new BinaryTree(value);
-        if( value < orignalTree.node ) {
-            orignalTree.left = addTree(orignalTree.left, value);
-        } else if( value > orignalTree.node ) {
-            orignalTree.right = addTree(orignalTree.right,value);
-        }
-        return orignalTree;
-    }
-    
 }
