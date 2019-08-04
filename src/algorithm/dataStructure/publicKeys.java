0b22414e5890e23befbbe2ca54bff31e38d12ac4
김석진 <7271kim@naver.com>
2019-08-04 14:28:31 +0900

    [2019.08.04] - 알고리즘

--- src/algorithm/dataStructure/publicKeys.java
+++ src/algorithm/dataStructure/publicKeys.java
@@ -1,9 +1,14 @@
+package algorithm.dataStructure;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class publicKeys {
     public static void main(String[] args) throws NumberFormatException, IOException {
+        /*
+         * https://www.acmicpc.net/problem/9322
+         * 알고리즘 문제
+         */
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n= Integer.parseInt(br.readLine());
         for( int example = 0; example < n; example++ ) {
