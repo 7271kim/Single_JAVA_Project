e537fbad439004743c2431f55fee7928b36ae045
김석진 <7271kim@naver.com>
2019-08-21 08:31:37 +0900

    테스트중

--- src/Main.java
+++ src/Main.java
@@ -9,20 +9,31 @@ import java.util.Map;
 public class Main {
     public static void main(String args[]){
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Map<String, String> total = new HashMap<String, String>();
-        Map<String, String> count = new HashMap<String, String>();
+        Map<String, Integer> total = new HashMap<String, Integer>();
         try {
             int totalNumber = Integer.parseInt(br.readLine());
+            Map<String, Integer> count = new HashMap<String, Integer>();
             for (int index = 0; index < totalNumber; index++) {
                 int nextNumber = Integer.parseInt(br.readLine());
-                for (int indexInner = 0; index < nextNumber; indexInner++) {
+                for (int indexInner = 0; indexInner < nextNumber; indexInner++) {
                     String[] items = br.readLine().split(" ");
                     String item    = items[0];
                     String group   = items[1];
-                    if( total.containsKey(items[0]) ) {
-                        
+                    System.out.println(item);
+                    System.out.println(group);
+                    System.out.println(total.containsKey(item));
+                    if( total.containsKey(item) ) {
+                        count.put(group, count.get(group)+1);
+                    } else {
+                        total.put(item, 1);
+                        count.put(group, 1);
                     }
                 }
+                int result = 1;
+                for (String group : count.keySet()) {
+                    result *= (count.get(group)+1);
+                }
+                System.out.println(result-1);
             }
         } catch (Exception e) {
         } 
