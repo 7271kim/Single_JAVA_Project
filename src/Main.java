deb02e955d8d1844b16daed5b51e81f077dee5c2
김석진 <7271kim@naver.com>
2019-08-20 19:01:48 +0900

    [2019.08.20] 다시 문제풀어본느 중

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,28 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String args[]){
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Map<String, String> total = new HashMap<String, String>();
+        Map<String, String> count = new HashMap<String, String>();
         try {
-            String[] input   = br.readLine().split(" ");
-            ArrayList<Integer> sorted = new ArrayList<Integer>();
-            for (int index = 0; index < input.length; index++) {
-                sorted.add(Integer.parseInt(input[index]));
+            int totalNumber = Integer.parseInt(br.readLine());
+            for (int index = 0; index < totalNumber; index++) {
+                int nextNumber = Integer.parseInt(br.readLine());
+                for (int indexInner = 0; index < nextNumber; indexInner++) {
+                    String[] items = br.readLine().split(" ");
+                    String item    = items[0];
+                    String group   = items[1];
+                    if( total.containsKey(items[0]) ) {
+                        
+                    }
+                }
             }
-            Collections.sort( sorted );
-            System.out.println(sorted.get( (sorted.size())/2));
         } catch (Exception e) {
         } 
         
