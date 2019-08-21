d1bd83eef35648942272e4ddf05312fcc8ea49bd
김석진 <7271kim@naver.com>
2019-08-21 20:37:50 +0900

    [2019.08.21] - tree 재정리 해야됨

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,182 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String args[]){
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        //Scanner sc = new Scanner(System.in);
-        
         try {
-            String inputData = br.readLine();
-            TempTree tree = new TempTree(Integer.parseInt(inputData));
-            while ( (inputData = br.readLine()) != null && inputData.length() != 0) {   
-                tree.add(tree, Integer.parseInt(inputData));
-            }
-            tree.post();
+             int exams = Integer.parseInt(br.readLine());
+            for (int exam = 0; exam < exams; exam++) {
+                String[] dvdShow = br.readLine().split(" ");
+                String[] shows   = br.readLine().split(" ");
+                int dvd     = Integer.parseInt(dvdShow[0]);
+                int show    = Integer.parseInt(dvdShow[1]);
+                int[] dvdPosition = new int[dvd+1];
+                int[] treeData  = new int[ dvd + show + 1 ];
+                for (int index = 1; index <= dvd; index++) {
+                    treeData[show+index] = 1;
+                    dvdPosition[index] = show+index;
+                }
+                String result = "";
+                IndexTree tree = new IndexTree(treeData);
+                for (int index = 0; index < shows.length; index++) {
+                    int item = Integer.parseInt(shows[index]);
+                    result += tree.sumTotal(dvdPosition[item]-1);
+                    if(index != shows.length-1) result += " ";
+                    tree.printOriginal();
+                    tree.update(show, 1);
+                    tree.printOriginal();
+                    tree.update(dvdPosition[item], 0);
+                    tree.printOriginal();
+                    
+                    dvdPosition[item] = show;
+                    show--;
+                }
+                System.out.println(result);
+            } 
         } catch (Exception e) {
         } 
         
     }
 }
-class TempTree {
-    int root;
-    TempTree left;
-    TempTree right;
-    public TempTree ( int root ) {
-        this.root  = root;
-        this.left  = null;
-        this.right = null;
+
+class BinaryIndexTree{
+    int tree [];
+    int orignalSize;
+    int treeLength;
+    
+    public BinaryIndexTree( int[] orignal ) {
+        orignalSize = orignal.length;
+        tree = new int[orignalSize*2+1];
+        treeLength = tree.length-orignalSize;
+        for (int index = 0; index < orignal.length; index++) {
+            update(index, orignal[index]);
+        }
     }
-    public TempTree add(TempTree tree , int next) {
-        if(tree == null) return new TempTree(next);
+    public void update(int index, int value) {
+        int orignalPosision = orignalSize+index+1;
+        int beforeValue     = tree[orignalPosision];
+        //System.out.println("orignalPosision" + orignalPosision);
+        //System.out.println("beforeValue" + beforeValue);
+        index += 1;
+        tree[orignalPosision]   = value;
+        while (index < treeLength) {
+            tree[index] = tree[index] - beforeValue + value;
+            index += (index & -index); 
+        }
+    }
+    public int sumTotal(int index) {
+        int res = 0;
+        while (index > 0) {
+            res += tree[index];
+            index &= index-1;
+        }
+        return res;
+    }
+    
+    public int sumInterVal(int start , int end) {
+        return sumTotal(end) - sumTotal(start-1);
+    }
+    
+    public void printTotal() {
+        for (int index = 0; index < tree.length; index++) {
+            System.out.print(tree[index] + " ");
+        }
+        System.out.println();
+    }
+    public void printFenwick() {
+        for (int index = 0; index < treeLength; index++) {
+            System.out.print(tree[index] + " ");
+        }
+        System.out.println();
+    }
+    public void printOriginal() {
+        for (int index = treeLength; index < tree.length; index++) {
+            System.out.print(tree[index] + " ");
+        }
+        System.out.println();
+    }
+}
+
+class IndexTree {
+    private int data[];
+    private int originalStart;
+    
+    public IndexTree( int[] orignal ) {
+        int originalSize = orignal.length;
+        originalStart = 1;
         
-        if( next < tree.root ) tree.left  = add(tree.left, next);
-        if( next > tree.root ) tree.right = add(tree.right, next);
+        while (originalStart < originalSize)
+            originalStart <<= 1;
         
-        return tree;
+        data = new int[originalStart*2];
+        
+        for( int index = 0; index < orignal.length; index++ ) {
+            update( index, orignal[index] );
+        }
     }
-    public void post() {
-        if(left != null) left.post();
-        if(right != null) right.post();
-        System.out.println(root);
+    
+    public void update( int index, int value) {
+        index += originalStart;
+        int beforeValue = data[index];
+        while(index > 0) {
+            data[index] = data[index] - beforeValue + value;
+            index>>=1;
+        }
     }
-    public void pre() {
-        System.out.println(root);
-        if(left != null) left.pre();
-        if(right != null) right.pre();
+    
+    public int sumTotal( int end ) {
+        int start = 1;
+        start += originalStart;
+        end += originalStart;
+        int sum = 0;
+        while ( start < end ) {
+            if( start%2 == 1) {
+                sum += data[start]; 
+                start++;
+            }
+            if( end%2 == 0) {
+                sum += data[end];
+                end--;
+            }
+            start>>=1;
+            end>>=1;
+        }
+        if( start == end ) {
+            sum += data[start];
+        }
+        
+        return sum;
+    }
+   public int sumInterVal( int start, int end ) {
+       start += originalStart;
+       end += originalStart;
+       int sum = 0;
+       while ( start < end ) {
+           if( start%2 == 1) {
+               sum += data[start]; 
+               start++;
+           }
+           if( end%2 == 0) {
+               sum += data[end];
+               end--;
+           }
+           start>>=1;
+           end>>=1;
+       }
+       if( start == end ) {
+           sum += data[start];
+       }
+       
+       return sum;
+   }
+   
+    public void printOriginal() {
+        for (int index = 0; index < data.length; index++) {
+            System.out.print(data[index] + " ");
+        }
+        System.out.println();
     }
 }
