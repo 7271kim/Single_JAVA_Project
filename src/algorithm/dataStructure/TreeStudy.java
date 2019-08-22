44e7e36225eaf95ac92b7aa48f479e069fbef8a1
김석진 <7271kim@naver.com>
2019-08-22 11:53:08 +0900

    [2019.08.22]  - 기존 팬윅트리는 4NlogN인데  - 커스텀한것은 N + 4NlogN이라 시간초과가 발생. 해당부분을 위해 오리지널도 당장 쓸수있도록 커스컴

--- src/algorithm/dataStructure/TreeStudy.java
+++ src/algorithm/dataStructure/TreeStudy.java
@@ -228,62 +228,91 @@ class BinaryIndexTree{
     }
 }
 
-class FenwickTreeOrignal{
+class FenwickTreeOriganl{
     int tree [];
     
-    public FenwickTreeOrignal(int size) {
+    public FenwickTreeOriganl(int size) {
         tree = new int[size];
     }
-    public void update(int p, int val) {
-        while (p < tree.length) {
-            tree[p] += val;
-            p += p & (-p);
+    public void update(int index, int val) {
+        int orignal  = sumTotal( index)-sumTotal( index-1);
+        int increase = val - orignal;
+        while ( index < tree.length) {
+            tree[ index ] += increase;
+            index +=  index & (- index);
         }
     }
-    public int sum(int p) {
+    
+    public int sumTotal(int endIndex) {
         int res = 0;
-        while (p > 0) {
-            res += tree[p];
-            p &= p-1;
+        while (endIndex > 0) {
+            res += tree[endIndex];
+            endIndex &= endIndex-1;
         }
         return res;
+    }
+    
+    public int sumInterVal(int startIndex, int endIndex) {
+        return sumTotal(endIndex) - sumTotal(startIndex);
+    }
+    
+    public void printOriginal() {
+        for (int index = 0; index < tree.length; index++) {
+            System.out.print(tree[index] + " ");
+        }
+        System.out.println();
     }
 }
 
 class IndexTreeOrignal{
     int tree [];
+    int originalStart;
     
-    public IndexTreeOrignal(int size) {
-        tree = new int[size];
+    public IndexTreeOrignal(int originalSize) {
+        originalStart = 1;
+        while (originalStart < originalSize)
+            originalStart <<= 1;
+        tree = new int[originalStart*2];
+    }
+    public int sumTotal ( int endIndex ) {
+        return sumInterVal(0, endIndex);
     }
     
-    public int sum(int b, int c) {
+    public int sumInterVal(int startIndex, int endIndex) {
         int sum = 0;
-        while (b < c) {
-            if ((b & 1) == 1) {
-                sum += tree[b];
-                b++;
+        startIndex += originalStart;
+        endIndex += originalStart;
+        while (startIndex < endIndex) {
+            if ((startIndex & 1) == 1) {
+                sum += tree[startIndex];
+                startIndex++;
             }
-            if ((c & 1) == 0) {
-                sum += tree[c];
-                c--;
+            if ((endIndex & 1) == 0) {
+                sum += tree[endIndex];
+                endIndex--;
             }
-            b /= 2;
-            c /= 2;
+            startIndex /= 2;
+            endIndex /= 2;
         }
-        if (b == c)
-            sum += tree[b];
+        if (startIndex == endIndex)
+            sum += tree[startIndex];
         return sum;
     }
  
-    public void update(int idx, int val) {
-        int minus = tree[idx];
-        int P = idx;
-        while (P != 0) {
-            tree[P] = tree[P] - minus + val;
-            P /= 2;
+    public void update(int index, int val) {
+        index += originalStart;
+        int minus = tree[index];
+        int pointer = index;
+        while (pointer != 0) {
+            tree[pointer] = tree[pointer] - minus + val;
+            pointer /= 2;
         }
     }
+    
+    public void printOriginal() {
+        for (int index = 0; index < tree.length; index++) {
+            System.out.print(tree[index] + " ");
+        }
+        System.out.println();
+    }
 }
-
-
