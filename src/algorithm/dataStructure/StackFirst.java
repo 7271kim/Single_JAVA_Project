aa61865d6ba7210b562a390d98b047f1129e3e80
seokjin <7271kim@naver.com>
2019-08-25 16:50:44 +0900

    [2019.08.25]  - stack java에서 제공해주는 것 있음

--- src/algorithm/dataStructure/StackFirst.java
+++ src/algorithm/dataStructure/StackFirst.java
@@ -1,3 +1,4 @@
+package algorithm.dataStructure;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -5,20 +6,37 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
+import java.util.Stack;
 
 /*
  * https://www.acmicpc.net/problem/10828
  * 스택 - JAVA에서 제공해주는 것이 있음
  */
 
-public class Main {
+public class StackFirst {
     public static void main(String args[]){
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        //Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         try {
             int number = Integer.parseInt(br.readLine());
-            StackMy stack = new StackMy();
-            for (int index = 0; index < number; index++) {
+            Stack<Integer> A = new Stack<Integer>();
+            for(int T = sc.nextInt(); T > 0; T--) {
+                String s = sc.next();
+                if(s.equals("push"))
+                    A.push(sc.nextInt());
+                if(s.equals("pop"))
+                    System.out.println(A.isEmpty()?-1:A.pop());
+                if(s.equals("size"))
+                    System.out.println(A.size());
+                if(s.equals("empty"))
+                    System.out.println(A.isEmpty()?1:0);
+                if(s.equals("top"))
+                    System.out.println(A.isEmpty()?-1:A.peek());
+            }
+            
+            //내꺼 
+            /* for (int index = 0; index < number; index++) {
                 String order = br.readLine();
                 if( order.indexOf("push") > -1 ) {
                     int inputNumber = Integer.parseInt(order.split(" ")[1]);
@@ -32,13 +50,14 @@ public class Main {
                 } else if (order.indexOf("top") > -1) {
                     stack.top();
                 }
-            }
+            }*/
         } catch (Exception e) {
             System.out.println(e);
         } 
     }
 }
 
+// 이건 내꺼 
 class StackMy{
     ArrayList<Integer> stack;
     
