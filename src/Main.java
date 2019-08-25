b337c2cab2d1f53192715fc58c134cd8723ad295
seokjin <7271kim@naver.com>
2019-08-25 16:57:54 +0900

    [2019.08.25]  - 스텍 추가

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,11 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Stack;
 
 /*
- * https://www.acmicpc.net/problem/10828
- * 스택 - JAVA에서 제공해주는 것이 있음
+ * https://www.acmicpc.net/problem/10773
+ * 스택
  */
 
 public class Main {
@@ -17,61 +18,20 @@ public class Main {
         //Scanner sc = new Scanner(System.in);
         try {
             int number = Integer.parseInt(br.readLine());
-            StackMy stack = new StackMy();
+            Stack<Integer> stack = new Stack<Integer>();
+            int sum    = 0;
             for (int index = 0; index < number; index++) {
-                String order = br.readLine();
-                if( order.indexOf("push") > -1 ) {
-                    int inputNumber = Integer.parseInt(order.split(" ")[1]);
-                    stack.push(inputNumber);
-                } else if( order.indexOf("pop") > -1 ) {
-                    stack.pop();
-                } else if (order.indexOf("size") > -1) {
-                    stack.size();
-                } else if (order.indexOf("empty") > -1) {
-                    stack.empty();
-                } else if (order.indexOf("top") > -1) {
-                    stack.top();
+                int line = Integer.parseInt(br.readLine());
+                if( line == 0 ) {
+                    sum -= stack.pop();
+                } else {
+                    sum += line;
+                    stack.push(line);
                 }
             }
+            System.out.println(sum);
         } catch (Exception e) {
             System.out.println(e);
         } 
-    }
-}
-
-class StackMy{
-    ArrayList<Integer> stack;
-    
-    public StackMy() {
-        stack = new ArrayList<Integer>();
-    }
-    public void push( int input ) {
-        stack.add(input);
-    }
-    public void pop() {
-        if(stack.isEmpty()) {
-            System.out.println(-1);
-        } else {
-            System.out.println(stack.get(stack.size()-1));
-            stack.remove(stack.size()-1);
-        }
-        
-    }
-    public void size() {
-        System.out.println(stack.size());
-    }
-    public void empty() {
-        if(stack.isEmpty()) {
-            System.out.println(1);
-        } else {
-            System.out.println(0);
-        }
-    }
-    public void top() {
-        if(stack.isEmpty()) {
-            System.out.println(-1);
-        } else {
-            System.out.println(stack.get(stack.size()-1));
-        }
     }
 }
