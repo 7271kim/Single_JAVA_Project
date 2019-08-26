ab911fde6be19b7df654de9e6cc4a1b2edc1a705
김석진 <7271kim@naver.com>
2019-08-26 13:39:31 +0900

    [2019.08.26]  - 시간초과

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,44 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 import java.util.Stack;
 
 /*
- * https://www.acmicpc.net/problem/1874
- * 스택 - JAVA에서 제공해주는 것이 있음
- * 시작과 종료조건에 대한 고민
+ * https://www.acmicpc.net/problem/17298
+ * 로직에 대한 고민
  */
 
 public class Main {
     public static void main(String args[]){
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        //Scanner sc = new Scanner(System.in);
+        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
         try {
-            int number = Integer.parseInt(br.readLine());
-            StringBuilder result = new StringBuilder();
-            Stack<Integer> stack = new Stack<Integer>();
-            int lastNumber       = 1;
-            for (int index = 0; index < number; index++) {
-               int line = Integer.parseInt(br.readLine());
-               while(lastNumber <= line) {
-                   result.append("+\n");
-                   stack.add(lastNumber++);
-               }
-               if( stack.isEmpty() ) {
-                   result = new StringBuilder("NO");
-                   break;
-               } else {
-                   while(stack.peek() >= line) {
-                       result.append("-\n");
-                       stack.pop();
-                       if(stack.isEmpty()) break;
-                   }
-               }
+            int total       = sc.nextInt();
+            int[] A     = new int[ 1000001]; // 구하는 A[i] 조합 인덱스가 inputNum
+            int[] inputNum = new int[total]; // 들어오는 배열
+            int min     = 0; // 업데이트해야할 최소값
+            Stack<Integer> stack = new Stack<Integer>(); // 업데이트 해야할 것들
+            
+            for (int index = 0; index < total; index++) {
+                int number = sc.nextInt();
+                inputNum[index] = number;
+                
+                if(A[number] == 0) A[number] =-1;
+                if( number > min && stack.size() > 0) {
+                    for (int stackIndex = stack.size()-1; stackIndex >= 0; stackIndex--) {
+                        int stackNumb = stack.get(stackIndex);
+                        if( stackNumb < number ) {
+                            A[stack.get(stackIndex)] = number;
+                            stack.pop();
+                            if(stack.size() == 0) break;
+                        }
+                    }
+                }
+                stack.add(number);
+                min = number;
             }
-            System.out.println(result);
+            
+            for (int index = 0; index < inputNum.length; index++) {
+                System.out.print(A[inputNum[index]] + " ");
+            }
         } catch (Exception e) {
             System.out.println(e);
         } 
