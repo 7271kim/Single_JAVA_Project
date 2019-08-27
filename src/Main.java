1596f7fbf690099111707bba6e76ce573240c93d
김석진 <7271kim@naver.com>
2019-08-27 17:00:44 +0900

    [2019.08.27]  - Deque 연습

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 import java.util.Stack;
 
 /*
- * https://www.acmicpc.net/problem/10866
+ * https://www.acmicpc.net/problem/1021
  * Deque 
  *
  */
@@ -15,37 +15,26 @@ import java.util.Stack;
 public class Main {
     public static void main(String args[]){
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        //Scanner sc = new Scanner(System.in);
-        //  1 : peak,poll
-        //  2
-        //  3
         try {
-            int total = Integer.parseInt(br.readLine());
+            String[] firstLine = br.readLine().split(" ");
+            String[] secondLine = br.readLine().split(" ");
+            int total = Integer.parseInt(firstLine[0]);
             Deque<Integer> deque   = new LinkedList<Integer>();
-            int last    = -1;
-            for (int index = 0; index < total; index++) {
-                String line = br.readLine();
-                if( line.indexOf("push_front") > -1 ) {
-                    int temp =Integer.parseInt(line.split(" ")[1]); 
-                    deque.addFirst(temp);
-                    last = temp;
-                } else if( line.indexOf("push_back") > -1 ) {
-                    int temp =Integer.parseInt(line.split(" ")[1]); 
-                    deque.addLast(temp);
-                } else if( line.indexOf("pop_front") > -1 ) {
-                    System.out.println(deque.isEmpty()?-1:deque.pop());
-                } else if( line.indexOf("pop_back") > -1 ) {
-                    System.out.println(deque.isEmpty()?-1:deque.removeLast());
-                } else if( line.indexOf("size") > -1 ) {
-                    System.out.println(deque.size());
-                } else if( line.indexOf("front") > -1 ) {
-                    System.out.println(deque.isEmpty()?-1:deque.peek());
-                }  else if( line.indexOf("back") > -1 ) {
-                    System.out.println(deque.isEmpty()?-1:deque.peekLast());
-                } else if( line.indexOf("empty") > -1 ) {
-                    System.out.println(deque.isEmpty()?1:0);
-                } 
+            int result = 0;
+            for (int index = 1; index <= total; index++) 
+                deque.addLast(index);
+            for (int index = 0; index < secondLine.length; index++) {
+                int number = Integer.parseInt(secondLine[index]);
+                int position = 0;
+                while(number != deque.peek()) {
+                    position++;
+                    deque.addLast(deque.pop());
+                }
+                int backSize = deque.size()-position; // 앞으로 돌때, 뒤로 돌때 크기 확인
+                result += position < backSize ? position : backSize;
+                deque.pop();
             }
+            System.out.println(result);
         } catch (Exception e) {
             System.out.println(e);
         } 
