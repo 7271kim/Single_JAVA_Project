d039696c15fb191283205e942b48f30e8de5c64a
김석진 <7271kim@naver.com>
2019-08-28 17:36:08 +0900

    [2019.08.28]  - Deque 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -7,7 +8,7 @@ import java.util.Scanner;
 import java.util.Stack;
 
 /*
- * https://www.acmicpc.net/problem/1021
+ * https://www.acmicpc.net/problem/5430
  * Deque 
  *
  */
@@ -16,27 +17,85 @@ public class Main {
     public static void main(String args[]){
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         try {
-            String[] firstLine = br.readLine().split(" ");
-            String[] secondLine = br.readLine().split(" ");
-            int total = Integer.parseInt(firstLine[0]);
-            Deque<Integer> deque   = new LinkedList<Integer>();
-            int result = 0;
-            for (int index = 1; index <= total; index++) 
-                deque.addLast(index);
-            for (int index = 0; index < secondLine.length; index++) {
-                int number = Integer.parseInt(secondLine[index]);
-                int position = 0;
-                while(number != deque.peek()) {
-                    position++;
-                    deque.addLast(deque.pop());
+            //String[] firstLine = br.readLine().split(" ");
+            //String[] secondLine = br.readLine().split(" ");
+            //int total = Integer.parseInt(firstLine[0]);
+            int total = Integer.parseInt(br.readLine());
+            for (int totalIndex = 0; totalIndex < total; totalIndex++) {
+                String[] order = br.readLine().split("");
+                int secondLine = Integer.parseInt(br.readLine());
+                String[] arrayNumber = br.readLine().split(",");
+                AC acData = new AC(arrayNumber);
+                Boolean result = true;
+                if( order.length + secondLine <= 700000 ) {
+                    for (int orderIndex = 0; orderIndex < order.length; orderIndex++) {
+                        if( order[orderIndex].indexOf("R") > -1 ) {
+                            acData.reverse();
+                        } else {
+                            if(!acData.destroy()) {
+                                result = false;
+                                break;
+                            }
+                            
+                        }
+                    }
+                } else {
+                    result = false;
                 }
-                int backSize = deque.size()-position; // 앞으로 돌때, 뒤로 돌때 크기 확인
-                result += position < backSize ? position : backSize;
-                deque.pop();
+                if(result) {
+                    acData.print();
+                } else {
+                    System.out.println("error");
+                }
             }
-            System.out.println(result);
         } catch (Exception e) {
             System.out.println(e);
         } 
+    }
+}
+
+class AC {
+    Deque<Integer> data = new LinkedList<Integer>();
+    
+    public AC ( String[] arrayNumber ) {
+        for (int index = 0; index < arrayNumber.length; index++) {
+            String num  = arrayNumber[index].replaceAll("\\[", "").replaceAll("\\]", "");
+            if(!num.equals("")) {
+                data.add(Integer.parseInt(num));
+            }
+        }
+    }
+    
+    public void reverse() {
+        Deque<Integer> temp = new LinkedList<Integer>();
+        for (int index = data.size()-1; index >= 0; index--) {
+            temp.addLast(data.pollLast());
+        }
+        data = temp;
+    }
+    
+    public Boolean destroy() {
+       Boolean result = true;
+       if(!data.isEmpty()) {
+           data.pollFirst(); 
+       } else {
+           result = false;
+       }
+       return result;
+    }
+    
+    public void print() {
+        StringBuilder result = new StringBuilder();
+        result.append("[");
+        int totalSize = data.size();
+        for (int index = 0; index < totalSize; index++) {
+            result.append(data.pollFirst());
+            if( data.size() != 0 ) {
+                result.append( ",");
+            } else {
+                result.append( "]");
+            }
+        }
+        System.out.println(result);
     }
 }
