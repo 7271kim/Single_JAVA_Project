c97c9a5da59e507122b7a10b2c6b299e201620e2
김석진 <7271kim@naver.com>
2019-09-03 23:23:46 +0900

    [2019.09.03]  - 계차수열 다시

--- src/algorithm/math/Sequence2.java
+++ src/algorithm/math/Sequence2.java
@@ -4,7 +4,7 @@ package algorithm.math;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
-public class Main{
+public class Sequence2{
 	public static void main(String args[]){
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Scanner sc = new Scanner(System.in);
--- src/ok/beak/Beak1193Sequence2.java
+++ src/ok/beak/Beak1193Sequence2.java
@@ -7,8 +7,7 @@ import java.util.Map;
 import java.util.Scanner;
 
 /*
- * https://www.acmicpc.net/problem/1316
- * Deque 
+ * https://www.acmicpc.net/problem/1193
  *
  */
 
