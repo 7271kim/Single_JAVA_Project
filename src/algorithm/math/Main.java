a962993cbdd566f8fcafd433f9f4f446562f572f
김석진 <7271kim@naver.com>
2019-09-03 23:04:05 +0900

    [2019.09.03]  - 계차수열 다시 중

--- src/algorithm/math/Main.java
+++ src/algorithm/math/Main.java
@@ -1,24 +1,36 @@
 package algorithm.math;
 
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
 public class Main{
 	public static void main(String args[]){
-		/*
-		 * https://www.acmicpc.net/problem/2588
-		 * 알고리즘 문제
-		 * https://7271kim.github.io/algorithm/2019/06/29/algorithm.html
-		 * (헤석)
-		 */
-		
-		Scanner sc = new Scanner(System.in);
-		int a,b,c;
-		a = sc.nextInt();
-		b = sc.nextInt();
-		System.out.println(a*(b%10));
-		System.out.println(a*(b%100/10));
-		System.out.println(a*(b%1000/100));
-		System.out.println(a*b);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
+        try {
+            
+            
+            int total   = sc.nextInt();
+             // 계차 수열의 일반항 따위는 필요없음. 말 그대로 표현만 하면 된다.
+             // a의 n은  n방의 최대로 들어갈 수 있는 수.
+             // a1 = 1 
+             // a2 = a1 + 6 
+             // a3 = a2 + 6 + 6
+             // a4 = a3 + 6 + 6 + 6
+             // . . . . .
+            int count  = 1;
+            int first  = 1;
+            int d      = 6;
+            while( true ) {
+                first += d*( count -1 );
+                if( total <= first ) break; // 각 구간의 끝 단계보다 작거나 같아야함 : 자기의 구간 
+                count++;
+            }
+            System.out.println(count);
+        } catch (Exception e) {
+            System.out.println(e);
+        } 
 	}
 }
 
---
+++ src/algorithm/math/Sequence.java
@@ -0,0 +1,40 @@
+package algorithm.math;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/*
+ * https://www.acmicpc.net/problem/2292
+ * Deque 
+ *
+ */
+
+public class Sequence {
+    public static void main(String args[]){
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
+        try {
+            
+            
+            int total   = sc.nextInt();
+             // 계차 수열의 일반항 따위는 필요없음. 말 그대로 표현만 하면 된다.
+             // a의 n은  n방의 최대로 들어갈 수 있는 수.
+             // a1 = 1 
+             // a2 = a1 + 6 
+             // a3 = a2 + 6 + 6
+             // a4 = a3 + 6 + 6 + 6
+             // . . . . .
+            int count  = 1;
+            int first  = 1;
+            int d      = 6;
+            while( true ) {
+                first += d*( count -1 );
+                if( total <= first ) break; // 각 구간의 끝 단계보다 작거나 같아야함 : 자기의 구간을 찾는법. a2 = 7까지임 : 7보다 작거나 같다. 
+                count++;
+            }
+            System.out.println(count);
+        } catch (Exception e) {
+            System.out.println(e);
+        } 
+    }
+}
