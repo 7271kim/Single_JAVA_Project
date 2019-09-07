4543a6da425e4ec98f1a77ecc4a3969cb81e1a5f
김석진 <7271kim@naver.com>
2019-09-07 20:34:44 +0900

    [2019.09.07]  - 카카오 2번 문제

--- src/Solution.java
+++ src/Solution.java
@@ -1,49 +1,65 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-// 1번문제 풀이
+// 문제 2번 - 시키는 대로 하기
 class Solution {
-    public int solution(String s) {
-        int totalLength = s.length(); 
-        int maxDive     = totalLength/2;
-        int min         = totalLength;
-        for (int index = 1; index <= maxDive; index++) {
-            int result      = 0;
-            ArrayList<String> temp = new ArrayList<String>();
-            int start = 0;
-            int end   = index;
-            while( end < totalLength ) {
-                temp.add(s.substring( start, end ));
-                start = end;
-                end   += index;
-                if( end >= totalLength) {
-                    temp.add(s.substring( start, totalLength ));
-                } 
+    public String solution(String p) {
+        String answer = "";
+        answer = recall(p);
+        return answer;
+    }
+    
+    public String recall( String p ) {
+        if( p.equals("") ) return "";
+        //입력이 빈 문자열인 경우, 빈 문자열을 반환합니다. 
+        
+        int closed = 0;
+        int open   = 0;
+        String[] input = p.split("");
+        String result = "";
+        String first = "";
+        String second = "";
+        Boolean isPerfect = true;
+        
+        for (int index = 0; index < input.length; index++) {
+            String temp = input[index];
+            if( temp.equals("(") ) {
+                open++;
+            } else {
+                closed++;
+                if( open - closed < 0 ) isPerfect = false;
             }
-            Map<String, Integer> mapData = new HashMap<String, Integer>();
-            for (int tmepIndex = 0; tmepIndex < temp.size(); tmepIndex++) {
-                String text      = temp.get(tmepIndex);
-                result          += text.length();
-                if( !mapData.containsKey(text) ) {
-                    mapData.clear();
-                    mapData.put(text, 1);
-                } else {
-                    result -= text.length();
-                    int mapNumber   = mapData.get(text);
-                    if(mapNumber == 1 ) {
-                        result++;
-                    } else {
-                        int addSize     = String.valueOf((mapNumber + 1)).length();
-                        int beForeSize  = String.valueOf((mapNumber)).length();
-                        result +=  ( addSize - beForeSize);
-                    }
-                    mapData.put(text, mapNumber+1);
-                }
-            }
-            if( min > result ) {
-                min = result;
+            if( open == closed ) {
+                // 균형잡힌 괄호 문자열" u, v로 분리
+                first    = p.substring(0, index+1);;
+                second   = p.substring(index+1, p.length());
+                break;
             }
         }
-        return min;
+        if( isPerfect ) {
+            //올바른 괄호 문자열  이라면 문자열 v에 대해 1단계부터 다시 수행합니다. 
+            result += first;
+            result += recall(second);
+        } else {
+            // 가 "올바른 괄호 문자열"이 아니라면 아래 과정을 수행합니다
+            //빈 문자열에 첫 번째 문자로 '('를 붙입니다
+            result+= "(";
+            //문자열 v에 대해 1단계부터 재귀적으로 수행한 결과 문자열을 이어 붙입니다. 
+            result += recall(second);
+            // ')'를 다시 붙입니다. 
+            result+= ")";
+            
+            // u의 첫 번째와 마지막 문자를 제거하고, 나머지 문자열의 괄호 방향을 뒤집어서 뒤에 붙입니다. 
+            String[] temp = first.split("");
+            temp[0] = "";
+            temp[temp.length-1] = "";
+            for (int index = 0; index < temp.length; index++) {
+                String innerText = temp[index];
+                if( innerText.equals("(") ) {
+                    result+= ")";
+                } else if( innerText.equals(")")) {
+                    result+= "(";
+                }
+            }
+        }
+        
+        return result;
     }
 }
