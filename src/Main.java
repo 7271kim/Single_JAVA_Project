1a9fe3fca45b1732297c38aab8c6f1b2325eb9a3
김석진 <7271kim@naver.com>
2019-09-24 15:10:59 +0900

    [2019.09.24]  - 문제

--- src/Main.java
+++ src/Main.java
@@ -1,61 +1,43 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
-
-/*
- * https://www.acmicpc.net/problem/6064https://ggmouse.tistory.com/317
- * https://ggmouse.tistory.com/317
- * https://sexycoder.tistory.com/26
- * https://mygumi.tistory.com/325
- * 시간초과 소스
- */
-
 public class Main {
     public static void main(String args[]){
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        //Scanner sc = new Scanner(System.in);
-        //StringBuilder resultString = new StringBuilder();
-        try {
-            //String[] firstLine = br.readLine().split(" ");
-            //int total   = Integer.parseInt(firstLine[0]);
-            //int compare = Integer.parseInt(firstLine[1]);
-            //String[] secondeLine = br.readLine().split(" ");
-            //int total = Integer.parseInt(br.readLine());
-            //int total = sc.nextInt();
-            //String result = "mixed";
-            
-            int total = Integer.parseInt(br.readLine());
-            for (int index = 0; index < total; index++) {
-                String[] firstLine = br.readLine().split(" ");
-                int M = Integer.parseInt(firstLine[0]);
-                int N = Integer.parseInt(firstLine[1]);
-                int x = Integer.parseInt(firstLine[2]);
-                int y = Integer.parseInt(firstLine[3]);
-                int tempX = 1;
-                int tempY = 1;
-                int result = 1;
-                while( !(tempX == x && tempY == y) && !( tempX == M && tempY == N ) ) {
-                    result++;
-                    if( tempX < M ) {
-                        tempX++;
-                    } else {
-                        tempX = 1;
-                    } 
-                    if( tempY < N ) {
-                        tempY++;
-                    } else {
-                        tempY = 1;
-                    }
-                }
-                if( tempX == M && tempY == N && x != M &&  y != N ) result = -1;
-                System.out.println(result);
-            }
-            
-        } catch (Exception e) {
-            System.out.println(e);
-        } 
+        Solution test = new Solution();
+        Tree root = new Tree();
+        Tree rootLeft = new Tree();
+        Tree rootLeftLeft = new Tree();
+        Tree rootLeftRight = new Tree();
+        
+        Tree rootRigth = new Tree();
+        Tree rootRigthLeft = new Tree();
+        Tree rootRigthRight = new Tree();
+        
+        Tree rootRigthRightRight = new Tree();
+        
+        root.x = 1;
+        rootLeft.x = 3;
+        rootLeftLeft.x = 2;
+        rootLeftRight.x = 2;
+
+        rootRigth.x = 4;
+        rootRigthLeft.x = 2;
+        rootRigthRight.x = 2;
+        
+        rootRigthRightRight.x = 1;
+        
+        root.l = rootLeft;
+        
+        rootLeft.l = rootLeftLeft;
+        rootLeft.r = rootLeftRight;
+        
+        
+        root.r = rootRigth;
+        
+        rootRigth.l = rootRigthLeft;
+        rootRigth.r = rootRigthRight;
+        
+        rootRigthRight.r = rootRigthRightRight;
+        
+        
+        
+        System.out.println(test.solution(root));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,65 +1,39 @@
-// 문제 2번 - 시키는 대로 하기
 class Solution {
-    public String solution(String p) {
-        String answer = "";
-        answer = recall(p);
-        return answer;
+    public int solution(Tree T) {
+        
+        int[] containNumber = new int[50001];
+        int maxCount = 0;
+        
+        maxCount = check( T, maxCount , containNumber );
+        
+        return maxCount;
     }
     
-    public String recall( String p ) {
-        if( p.equals("") ) return "";
-        //입력이 빈 문자열인 경우, 빈 문자열을 반환합니다. 
+    public int check( Tree T, int maxCount, int[]containNumber ) {
+        int root = T.x;
+        int leftMax  = 0;
+        int rightMax = 0;
         
-        int closed = 0;
-        int open   = 0;
-        String[] input = p.split("");
-        String result = "";
-        String first = "";
-        String second = "";
-        Boolean isPerfect = true;
-        
-        for (int index = 0; index < input.length; index++) {
-            String temp = input[index];
-            if( temp.equals("(") ) {
-                open++;
-            } else {
-                closed++;
-                if( open - closed < 0 ) isPerfect = false;
-            }
-            if( open == closed ) {
-                // 균형잡힌 괄호 문자열" u, v로 분리
-                first    = p.substring(0, index+1);;
-                second   = p.substring(index+1, p.length());
-                break;
-            }
+        if( containNumber[root] > 0 ) {
+            containNumber[root] += 1;
+            return maxCount;
         }
-        if( isPerfect ) {
-            //올바른 괄호 문자열  이라면 문자열 v에 대해 1단계부터 다시 수행합니다. 
-            result += first;
-            result += recall(second);
-        } else {
-            // 가 "올바른 괄호 문자열"이 아니라면 아래 과정을 수행합니다
-            //빈 문자열에 첫 번째 문자로 '('를 붙입니다
-            result+= "(";
-            //문자열 v에 대해 1단계부터 재귀적으로 수행한 결과 문자열을 이어 붙입니다. 
-            result += recall(second);
-            // ')'를 다시 붙입니다. 
-            result+= ")";
-            
-            // u의 첫 번째와 마지막 문자를 제거하고, 나머지 문자열의 괄호 방향을 뒤집어서 뒤에 붙입니다. 
-            String[] temp = first.split("");
-            temp[0] = "";
-            temp[temp.length-1] = "";
-            for (int index = 0; index < temp.length; index++) {
-                String innerText = temp[index];
-                if( innerText.equals("(") ) {
-                    result+= ")";
-                } else if( innerText.equals(")")) {
-                    result+= "(";
-                }
-            }
+        containNumber[root] += 1;
+        maxCount++;
+        
+        if(T.l != null) {
+            leftMax = check( T.l, maxCount, containNumber );
+            containNumber[T.l.x] -= 1;
         }
         
-        return result;
+        if(T.r != null) {
+            rightMax = check( T.r, maxCount, containNumber );
+            containNumber[T.r.x] -= 1;
+        }
+        maxCount = maxCount < leftMax ? leftMax : maxCount;
+        maxCount = maxCount < rightMax ? rightMax : maxCount;
+        
+        return maxCount;
     }
+    
 }
