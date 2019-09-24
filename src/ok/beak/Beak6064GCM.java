67b8ec312674f72a29f030b5cdabd0ad98a90506
김석진 <7271kim@naver.com>
2019-09-25 00:07:47 +0900

    [2019.09.25]  - 시험 및 백준 정리

--- src/ok/beak/Beak6064GCM.java
+++ src/ok/beak/Beak6064GCM.java
@@ -1,3 +1,4 @@
+package ok.beak;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.math.BigDecimal;
@@ -12,7 +13,7 @@ import java.util.Scanner;
  * 시간초과 소스
  */
 
-public class Main {
+public class Beak6064GCM {
     public static void main(String args[]){
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         //Scanner sc = new Scanner(System.in);
