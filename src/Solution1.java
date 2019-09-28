77c423f2bbf0037eca337560d90bba0466d1c4fa
seokjin <7271kim@naver.com>
2019-09-28 13:57:14 +0900

    [2019.09.28] - EST를 코드리뷰

--- src/Solution1.java
+++ src/Solution1.java
@@ -1,3 +1,7 @@
+// solution("aaaaa") , solution("aaaaabb") 등 인풋이 들어왔을 때 연속된 3글자가 나오는 케이스는 초기화 필요하다고 판단하고 코딩을 진행  
+// 초기화는 연속된 3글자 이상인 경우는 2개로 모든것을 초기화
+// 기존 max와 비교하여 최대값 구함
+
 class Solution1 {
     public int solution(String S) {
        String[] temp  = S.split("");
@@ -7,17 +11,24 @@ class Solution1 {
        int countText  = 0;
        
        for (int index = 0; index < temp.length; index++) {
+           // 전체 카운트
            countText++;
+           // 글자가 나오는 케이스는 2가지 밖에 없음 a 혹은 b
            if( temp[index].equals("a") ) {
+               //연속된 a의 갯수 확인
                aCount++;
                if( aCount > 2 ) {
                    aCount    = 2;
+                   // 초기화. a가 연속되도 나올수 있는 conText크기는 2임
                    countText = 2;
                }
+               // 이전것이 b가 아니면 b 카운트는 초기화
                if( index != 0 && temp[index-1].equals("b") ) {
                    bCount     = 0;
                }
            } else {
+               //b도 동일한 방법으로 구현
+               // 반복 로직이라 더 효율적으로 구성할 수 있을것으로 보임
                bCount++;
                if( bCount > 2 ) {
                    bCount    = 2;
@@ -27,6 +38,7 @@ class Solution1 {
                    aCount     = 0;
                }
            }
+           // 기존 max카운트와 비교
            max = max < countText ? countText : max;
        }
        
--- src/Solution3.java
+++ src/Solution3.java
@@ -1,36 +1,62 @@
+
+/*
+1 1 0 0 
+1 1 0 0
+
+ 1행의 합은 U
+ 2행의 합은 L
+  각 열의 합은 C[열]이 문제의 조건이다.
+  
+*/
 import java.util.ArrayList;
 
 class Solution3 {
     public String solution(int U, int L, int[] C) {
         String result = "";
         StringBuilder resultString = new StringBuilder();
+        // 윗줄을 출력하기 위한 변수 선언
         int[] upper     = new int[C.length];
+        // 아랫줄을 출력하기 위한 변수 선언
         int[] lower     = new int[C.length];
+        
+        // C의 값이 1인것의 index를 담아두는 변수
         ArrayList<Integer> putIndex = new ArrayList<Integer>();
         
+        // C의 값이 1인 갯수 체크
         int oneCount  = 0;
+        
+        // 인풋 C에 대한 처리 
         for (int index = 0; index < C.length; index++) {
             int number = C[index];
+            
+            //C가 2일경우 upper 와 lower줄은 둘다 1이다
             if( number == 2) {
                 upper[index] = 1;
                 lower[index] = 1;
                 U--;
                 L--;
+                // 위아래 합을 하나씩 빼줌
                 if( U < 0 || L < 0 ) {
+                    // 만약 U와 L이 0보다 작아지면 더이상의 로직이 필요없음  불가능함
                     result = "IMPOSSIBLE";
                     break;
                 }
             } else if( number == 0 ) {
+              //C가 2일경우 upper 와 lower줄은 둘다 0이다
                 upper[index] = 0;
                 lower[index] = 0;
             } else {
+                // 1의 갯수 카운트
                 oneCount++;
                 putIndex.add(index);
             }
         }
+        
+        // 만약 1읠 갯수가 남은 U와 L의 합과 같아야 조건에 만족한다.
         if( oneCount != ( U+L ) ) {
             result = "IMPOSSIBLE";
         } else {
+            // 이제 정답 출력을 위한 로직 추가
             for (Integer index : putIndex) {
                 if( U > 0 ) {
                     upper[index] = 1;
