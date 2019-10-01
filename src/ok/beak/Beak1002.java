dcd6eb2950f809f7bb44c5cfe4ebeaf3887295a6
김석진 <7271kim@naver.com>
2019-10-01 19:27:07 +0900

    [2019.10.01] - 백준 문제 풀이

--- src/ok/beak/Beak1002.java
+++ src/ok/beak/Beak1002.java
@@ -1,3 +1,4 @@
+package ok.beak;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.math.BigDecimal;
@@ -10,7 +11,7 @@ import java.util.Scanner;
  * https://www.acmicpc.net/problem/1002
  */
 
-public class Main {
+public class Beak1002 {
     public static void main(String args[]){
         //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Scanner sc = new Scanner(System.in);
