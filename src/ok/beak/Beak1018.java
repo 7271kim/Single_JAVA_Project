8e44619e6570d870f0573fe52a44dd5bb08385dd
seokjin <7271kim@naver.com>
2019-10-22 17:25:43 +0900

    [2019.10.22] - BeakJun 다시하기

--- src/ok/beak/Beak1018.java
+++ src/ok/beak/Beak1018.java
@@ -1,3 +1,4 @@
+package ok.beak;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
@@ -11,7 +12,7 @@ import java.util.Scanner;
  * 
  */
 
-public class Main {
+public class Beak1018 {
     public static void main(String args[]){
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Scanner sc = new Scanner(System.in);
