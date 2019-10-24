0e5655a432d83cc1a24b1ddd4e6e718d403b112e
김석진 <7271kim@naver.com>
2019-10-24 21:46:59 +0900

    [2019.10.24] - 프로그래머스 생각보다 까다로움

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,20 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Scanner;
-
 /*
- * https://www.acmicpc.net/problem/10989
- * input 개수가 1000만개가 넘어가 메모리 초과가 발생 
- * 발생의 전환 >> N의 범위가 10000이므로 크기가 10000인 배열은 준비하고 정렬 되었다고 가정하고 count를 업해줌  
- * intput방식을 Scanner로 할 시 시간초과 발생
+ * https://www.acmicpc.net/problem/1436
+ * 종말의 숫자란 어떤 수에 6이 적어도 3개이상 연속으로 들어가는 수
+ * N번째 >> N-1에 666을 붙이기 >> X 
+ * 문제가 16661도 종말의 수임.
+ * 666 ~ 무한대로 while 돌면서 종말의 수 확인
+ * 종말의 수 확인법 >> 해당이 문제 >> int형에서 연속된 숫자 확인하는 방법 >> 나는 String변경 후 matches수행
+ * findCount가 받은 값이랑 같을 경우 해당 숫자를 출력
+ * O( N(최대2,666,799 )*indexOf시간 복잡도(tartget 길이(7) * 찾는문자열 길이(3)) ) >> 56,002,779 >> 10억 안넘어서 괜춘
+ * 
  */
 
 public class Main {
     public static void main(String args[]){
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Scanner sc = new Scanner(System.in);
-        StringBuilder resultString = new StringBuilder();
-        try {
-            //String[] firstLine = br.readLine().split(" ");
-            //int total   = Integer.parseInt(firstLine[0]);
-            //int compare = Integer.parseInt(firstLine[1]);
-            //String[] secondeLine = br.readLine().split(" ");
-            //int total = Integer.parseInt(br.readLine());
-            //int total = sc.nextInt();
-            int total = Integer.parseInt(br.readLine());
-            int[] input =new int[10001];
-            
-            for (int index = 0; index < total; index++) {
-                int number = Integer.parseInt(br.readLine());
-                input[number]++;
-            }
-            
-            for (int index = 0; index < input.length; index++) {
-                int count = input[index];
-                if( count != 0 ) {
-                    for (int indexInner = 0; indexInner < count; indexInner++) {
-                        resultString.append(index+"\n");
-                    }
-                }
-            }
-           System.out.println(resultString.toString());
-            
-        } catch (Exception e) {
-            System.out.println(e);
-        } 
+      Solution temp = new Solution();
+      String[] priorities = {"123", "456", "789"};
+      //System.out.println(temp.solution(priorities));
     }
 }
+
--- src/Solution.java
+++ src/Solution.java
@@ -1,59 +1,9 @@
-import java.util.*;
-
 class Solution {
-    public int solution(String[] bishops) {
-        int answer = 0;
-        int[][] board = new int[8][8];
-        for (int index = 0; index < bishops.length; index++) {
-            String[] temp = bishops[index].split("");
-            int x = 0;
-            String xString = temp[0];
-            if( xString.equals("A")) {
-                x = 0;
-            } else if( xString.equals("B")) {
-                x = 1;
-            } else if( xString.equals("C")) {
-                x = 2;
-            } else if( xString.equals("D")) {
-                x = 3;
-            } else if( xString.equals("E")) {
-                x = 4;
-            } else if( xString.equals("F")) {
-                x = 5;
-            } else if( xString.equals("G")) {
-                x = 6;
-            } else if( xString.equals("H")) {
-                x = 7;
-            } 
-            int y = Integer.valueOf(temp[1])-1;
-            board[x][y] = 1;
-            
-            check( board, x+1, y+1 , "upup");
-            check( board, x+1, y-1 , "updown");
-            check( board, x-1, y+1 , "downup");
-            check( board, x-1, y-1 , "downdown");
-        }
-        
-        for (int index = 0; index < board.length; index++) {
-            for (int indexInner = 0; indexInner < board[index].length; indexInner++) {
-                if(board[index][indexInner] == 0) answer++;
-            }
-        }
-        
-        return answer;
-    }
-    
-    public int[][] check( int[][] board, int x, int y , String position) {
-        if(x < 0 || y < 0 || x > 7 || y > 7 ||  board[x][y] == 1 ) return board;
-        
-        board[x][y] = 1;
-        
-        if(position.equals("upup")) board = check( board, x+1, y+1, "upup" );
-        if(position.equals("updown")) board = check( board, x+1, y-1, "updown" );
-        if(position.equals("downup")) board = check( board, x-1, y+1, "downup" );
-        if(position.equals("downdown")) board = check( board, x-1, y-1, "downdown" );
-        
-        return board;
-    }
-    
+  public int[] solution(int n, long k) {
+      int[] answer = new int[n];
+      for (int index = 0; index < n; index++) {
+          answer[index] = (int)Math.ceil(k/index+n);
+      }
+      return answer;
+  }
 }
