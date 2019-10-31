33395daefaa14c467b064288cb01aa231a4e2119
seokjin <7271kim@naver.com>
2019-10-31 12:03:37 +0900

    [2019.10.31] - 2차배열 그래프 수정

--- src/alorithm/dataStructureLow/MyGraphArray.java
+++ src/alorithm/dataStructureLow/MyGraphArray.java
@@ -1,24 +1,52 @@
 package alorithm.dataStructureLow;
 public class MyGraphArray {
     private int[][] graph;
+    private int size;
+    private int max = 10000000;
 
     public MyGraphArray( int size) {
-        graph = new int [size+1][size+1];
+        this.size = size+1;
+        graph = new int [this.size][this.size];
+        
+        for (int start = 1; start < size; start++) {
+            for (int end = 1; end < size; end++) {
+                if( start == end ) {
+                    graph[start][end] = 0;
+                } else {
+                    graph[start][end] = max;
+                }
+            }
+        }
     }
     
     public void push( int start, int end ) {
-        graph[start][end] = 1;
+        push(start,end,1);
+    }
+    
+    public void push( int start, int end, int weight ) {
+        graph[start][end] = weight;
     }
     
     public void print() {
-        for (int index = 1; index < graph.length; index++) {
-            System.out.println("정점 " + index + "의 인접리스트");
-            for (int indexInner = 0; indexInner < graph[index].length; indexInner++) {
-                if( graph[index][indexInner] == 1 ) {
-                    System.out.print( indexInner + " ");
+        System.out.print("\t\t");
+        for (int index = 1; index < size; index++) {
+            System.out.print("Index : " + index+ "\t");
+        }
+        System.out.println();
+        for (int start = 1; start < size; start++) {
+            System.out.print("Index : " + start+ "\t");
+            for (int end = 1; end < size; end++) {
+                if( graph[start][end] == max) {
+                    System.out.print("X" + "\t\t");
+                } else {
+                    System.out.print(graph[start][end] + "\t\t");
                 }
             }
             System.out.println();
         }
+    }
+    
+    public int[][] getGraph (){
+        return graph;
     }
 }
