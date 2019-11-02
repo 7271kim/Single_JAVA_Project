860b4a05ce242db606030abf214a3f40f33687d0
seokjin <7271kim@naver.com>
2019-11-02 17:25:00 +0900

    [2019.11.02]  - KAKAO60060 Trie 풀이

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String args[]){
         Solution temp  = new Solution();
         Solution3 temp3 = new Solution3();
+            
+        String[] words1   = new String[1000];
+        String[] queries1 = new String[1000];
+        Map<String, String> map = new HashMap<String, String>();
         
-          String[] words1   = new String[10000];
-        String[] queries1 = new String[10000];
-        
-        for (int count = 1; count < 10001; count++) {
+        int count = 1;
+        while( count < words1.length+1 ) {
             StringBuilder tempB = new StringBuilder();
             double random  = Math.random();
             int value2      = ( (int)(random*100) +1 ) % 40+1;
@@ -16,42 +21,51 @@ public class Main {
                 char aaa = (char)value;
                 tempB .append(aaa);
             }
-            words1[count-1] = tempB.toString();
+            if(map.containsKey(tempB.toString())) {
+                continue;
+            } else {
+                words1[count-1] = tempB.toString();
+                map.put(tempB.toString(), "aa");
+                count++;
+            }
         }
         
-        for (int count = 1; count < 10001; count++) {
+        count = 1;
+        while( count < queries1.length+1 ) {
             StringBuilder tempB = new StringBuilder();
-             if(count < 50) {
-                tempB.append("?");
-                double random3  = Math.random();
-                int value      = ( (int)(random3*100) +1 ) % 5;
-                for (int index = 0; index < value; index++) {
-                    tempB.append("?");
-                }
-            }
-            
-            
-            double random  = Math.random();
-            int value2      = ( (int)(random*100) +1 ) % 5;
-            
-            for (int index = 0; index < value2; index++) {
-                random  = Math.random();
-                int value      = ( (int)(random*100) +1 ) % 5 + 1 + 96;
-                char aaa = (char)value;
-                tempB .append(aaa);
-            }
-            
-            if(count >= 50) {
-                tempB.append("?");
-                double random3  = Math.random();
-                int value      = ( (int)(random3*100) +1 ) % 5;
-                for (int index = 0; index < value; index++) {
-                    tempB.append("?");
-                }
-            }
-            //if(tempB.toString() == "" || tempB.toString()== null ) tempB.append("?");
-            queries1[count-1] = tempB.toString();
+            if(count < 50) {
+               tempB.append("?");
+               double random3  = Math.random();
+               int value      = ( (int)(random3*100) +1 ) % 5;
+               for (int index = 0; index < value; index++) {
+                   tempB.append("?");
+               }
+           }
+           
+           
+           double random  = Math.random();
+           int value2      = ( (int)(random*100) +1 ) % 5;
+           
+           for (int index = 0; index < value2; index++) {
+               random  = Math.random();
+               int value      = ( (int)(random*100) +1 ) % 5 + 1 + 96;
+               char aaa = (char)value;
+               tempB .append(aaa);
+           }
+           
+           if(count >= 50) {
+               tempB.append("?");
+               double random3  = Math.random();
+               int value      = ( (int)(random3*100) +1 ) % 5;
+               for (int index = 0; index < value; index++) {
+                   tempB.append("?");
+               }
+           }
+           //if(tempB.toString() == "" || tempB.toString()== null ) tempB.append("?");
+           queries1[count-1] = tempB.toString();
+           count++;
         }
+        
         int[] second1 = temp3.solution(words1, queries1);
         int[] first1  = temp.solution(words1, queries1);
         
@@ -65,8 +79,8 @@ public class Main {
             }
         }
         
-        String[] words = { "aaaabbaaa"};
-        String[] queries = {"a????????"};
+        String[] words = { "frodo", "front", "frost", "frozen", "frame", "kakao" };
+        String[] queries = {"fro??", "????o", "fr???", "fro???", "pro?"};
         int[] first  = temp.solution(words, queries);
         int[] second = temp3.solution(words, queries);
         
