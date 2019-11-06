c4a85dad0845518968c6297ccbb5c7acdff91e1e
김석진 <7271kim@naver.com>
2019-11-07 00:38:56 +0900

    [2019.11.07]  - 낼 정리하기

--- src/Solution.java
+++ src/Solution.java
@@ -1,23 +1,33 @@
-//https://programmers.co.kr/learn/courses/30/lessons/12900 >> 풀어보기
+// https://programmers.co.kr/learn/courses/30/lessons/12900 >> 풀어보기
+// https://coding-all.tistory.com/2
+// https://do-rang.tistory.com/9
 class Solution {
+    int [][] memorization;
+    
   public int solution(int n) {
       int answer = 0;
       int maxTwo = n /2;
-      for (int index = 0; index < maxTwo; index++) {
-          answer += combination( index + ( n - maxTwo), index );
-      }
-      return answer %1000000007;
+      memorization = new int[n+1][n+1];
+      
+        /*for (int index = 0; index < maxTwo; index++) {
+          answer += combination( n, index );
+        }*/
+      
+      System.out.println(combination( 100, 2 ));
+      return answer;
   }
   
-  //nCr = n-1Cr-1 + n-1Cr
-  public int combination(long n, long r) { 
+  //nCr = n-1Cr-1 + n-1Cr , DP 응용
+  public int combination(int n, int r) { 
       
-      if(n == r || r == 0) return 1; 
+      if( memorization[n][r] != 0 ) return memorization[n][r];
       
-      long temp1 = combination(n - 1, r - 1);
-      long temp2 = combination(n - 1, r);
+      if(n == r || r == 0) return 1;
       
-      return (int)( temp1 + temp2 );
+      memorization[n][r]   = combination(n-1 , r-1) + combination(n-1 , r);
+      memorization[n][n-r] = memorization[n][r]; 
+      
+      return memorization[n][r];
   }
 
 }
