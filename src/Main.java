db81f1b113491fc667497de1619d812c9fe27270
김석진 <7271kim@naver.com>
2019-11-07 00:09:58 +0900

    [2019.11.07]  - 순열중

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String args[]){
         Solution temp = new Solution();
-        System.out.println(temp.solution(60000));
+        System.out.println(temp.solution(100));
         
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -4,11 +4,20 @@ class Solution {
       int answer = 0;
       int maxTwo = n /2;
       for (int index = 0; index < maxTwo; index++) {
-          boolean[] visited = new boolean[n];
-          //answer += combination( n, index );
+          answer += combination( index + ( n - maxTwo), index );
       }
       return answer %1000000007;
   }
   
+  //nCr = n-1Cr-1 + n-1Cr
+  public int combination(long n, long r) { 
+      
+      if(n == r || r == 0) return 1; 
+      
+      long temp1 = combination(n - 1, r - 1);
+      long temp2 = combination(n - 1, r);
+      
+      return (int)( temp1 + temp2 );
+  }
 
 }
