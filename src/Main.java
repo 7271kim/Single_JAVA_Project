43f1cd64e1c72966802ed97eff6e1dc66f922e30
seokjin <7271kim@naver.com>
2019-11-10 15:00:02 +0900

    [2019.11.10]  - 탐욕법

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,86 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String args[]){
+        
+        /*while( true ) {
+            Solution temp = new Solution();
+            Solution3 temp3 = new Solution3();
+            int n;
+            int[] lost;
+            int[] reserve;
+            
+            Map<Integer, Integer> lostMap = new HashMap<Integer, Integer>();
+            Map<Integer, Integer> lostReserve = new HashMap<Integer, Integer>();
+            
+            double random  = Math.random();
+            
+            n = ( (int)( random*100) + 1) % 30 + 1;
+            
+           
+            
+            
+            int count = 0;
+            int nextN = ( (int)( random*100) + 1) % n;
+            lost     = new int[nextN];
+            int[] lost2     = new int[nextN];
+            int[] lost3     = new int[nextN];
+            
+            while ( count < nextN ) {
+                random  = Math.random();
+                int value      = ( (int)(random*100) + 1) % n;
+                value = value == 0 ? value = 1 : value;
+                
+                if(!lostMap.containsKey(value)) {
+                    lostMap.put(value,value);
+                    lost[count] = value;
+                    count++;
+                } 
+            }
+            
+            count = 0;
+            nextN = ( (int)( random*100) + 1) % n;
+            reserve  = new int[nextN];
+            int[] reserve2     = new int[nextN];
+            int[] reserve3     = new int[nextN];
+            
+            while ( count < nextN ) {
+                random  = Math.random();
+                int value      = ( (int)(random*100) + 1) % n;
+                value = value == 0 ? value = 1 : value;
+                
+                if(!lostReserve.containsKey(value)) {
+                    lostReserve.put(value,value);
+                    reserve[count] = value;
+                    count++;
+                } 
+            }
+            
+            Arrays.sort(lost);
+            Arrays.sort(reserve);
+            
+            for (int index = 0; index < lost.length; index++) {
+                lost2[index] = lost[index];
+                lost3[index] = lost[index];
+            }
+            for (int index = 0; index < reserve.length; index++) {
+                reserve2[index] = reserve[index];
+                reserve3[index] = reserve[index];
+            }
+            
+            int my  = temp.solution(n, lost2, reserve2);
+            int nam = temp3.solution(n, lost3, reserve3);
+            if( my != nam && n < 8) {
+                System.out.println("다르다");
+            }
+        }*/
+        
         Solution temp = new Solution();
-        int n = 5;
-        int[] lost   = {2,4,5};
-        int[] reserve   = {1,3};
+        int n = 7;
+        int[] lost   = {3};
+        int[] reserve   = {1,2,5};
         System.out.println(temp.solution(n, lost, reserve));
     }
 }
--- src/Solution3.java
+++ src/Solution3.java
@@ -1,59 +1,20 @@
-import java.util.HashMap;
-
 class Solution3 {
-    int result = 0;
-    int banned_id_size   = 0;
-    HashMap<String, String> cheked = new HashMap<String, String>();
-    
-    public int solution( String[] user_id, String[] banned_id ) {
-        
-        banned_id_size = banned_id.length;
-        
-        recursive( user_id, banned_id, 0 );
-        
-        return result;
-    }
-    
-    public void recursive( String[] user_id, String[] banned_id, int count ) {
-        if( banned_id_size == count ) {
-            StringBuilder temp = new StringBuilder();
-            for (int index = 0; index < user_id.length; index++) {
-                if( user_id[index].equals("") )
-                    temp.append(index);
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        answer = n;
+ 
+        for(int i = 0; i < lost.length; i++) {
+            boolean rent = false;
+            int j = 0;
+            while(!rent) {
+                if(j == reserve.length)                   break;
+                if(lost[i] == reserve[j])                {reserve[j] = -1; rent=true;}
+                else if(lost[i] - reserve[j] == 1 )      {reserve[j] = -1; rent=true;}
+                else if(lost[i] - reserve[j] == -1)      {reserve[j] = -1; rent=true;}
+                else                                     {j++;                      }
             }
-            String resultKey = temp.toString();
-            if( !cheked.containsKey(resultKey) ) {
-                result++;
-                cheked.put(resultKey, resultKey);
-            }
-            
-            return;
+            if(!rent) answer--;
         }
-        
-        // 하나하나 검사
-        for (int index = count; index < banned_id.length; index++) {
-            String search = banned_id[index];
-            banned_id[index] = "";
-            
-            out : for (int indexUser = 0 ; !search.equals("") && indexUser < user_id.length; indexUser++) {
-                String thisText = user_id[indexUser];
-                if(thisText.length() != search.length()) {
-                    continue;
-                } else {
-                    for (int indexChar = 0; indexChar < thisText.length(); indexChar++) {
-                        char searchChar   = search.charAt(indexChar);
-                        char thisTextChar = thisText.charAt(indexChar);
-                        if( searchChar != '*' && searchChar != thisTextChar  ) {
-                            continue out ;
-                        }
-                    }
-                    
-                    user_id[indexUser] = "";
-                    recursive( user_id, banned_id, count+1);
-                    user_id[indexUser] = thisText;
-                }
-            }
-            banned_id[index] = search;
-        }
+        return answer;
     }
 }
