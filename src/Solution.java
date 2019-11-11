00bd24b2c9ed3580358be7ea95ef0cbcc92faf63
김석진 <7271kim@naver.com>
2019-11-11 23:16:50 +0900

    [2019.11.11]  - 윤호랑 푸는 중

--- src/Solution.java
+++ src/Solution.java
@@ -1,11 +1,15 @@
 //https://programmers.co.kr/learn/courses/30/lessons/12916 간단한거
 class Solution {
     boolean solution(String s) {
+        
         int[] data = new int[26];
+        
         s = s.toLowerCase();
+        
         for (int index = 0; index < s.length(); index++) {
             data[s.charAt(index) - 'a'] += 1;
         }
+        
         return data[15] == data[24];
     }
 }
