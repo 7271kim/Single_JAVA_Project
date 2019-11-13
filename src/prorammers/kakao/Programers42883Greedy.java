cfba969add5897350edd2a95d65f25a5c97107cd
seokjin <7271kim@naver.com>
2019-11-13 15:33:32 +0900

    [2019.11.13]  - 탐욕법 좀더 공부가 필요

--- src/prorammers/kakao/Programers42883Greedy.java
+++ src/prorammers/kakao/Programers42883Greedy.java
@@ -1,10 +1,11 @@
+package prorammers.kakao;
 import java.util.Stack;
 
 // https://programmers.co.kr/learn/courses/30/lessons/42883
 // 탐욕법 및 Stack에 대한 응용이 잘 안됨.
 // 핵심은 하나하나 지워가면서 4개가 되면 더이상 지울 수 없음. 이것에 대해 헷갈림.
 
-class Solution {
+class Programers42883Greedy {
     public String solution( String number, int k ) {
         int count = 0;
         int size = number.length();
