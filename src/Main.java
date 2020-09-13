83afacd713fa450a2c99c4d12aa539dcd8eb4e4b
seokjin <7271kim@naver.com>
2020-09-13 16:09:38 +0900

    [2020.09.13] 보글게임

--- src/Main.java
+++ src/Main.java
@@ -20,223 +20,73 @@ import alorithm.dataStructureLow.TreeWithLinkedList;
 import sun.tools.jconsole.MaximizableInternalFrame;
 
 public class Main {
-    private static void result( String[] people ) {
-        // 1. 끝점
-        // 2. 시작점
-        // 3. 개별 임시 결과 저장
-        // 4. 최종 결과
-        // 5. 다음 뽑을 것을 위한 index
-        
-        int end = 3;
-        int start  = 0;
-        String[] tempResult = new String[end];
-        int loopStartIndex = 0; 
-        ArrayList<String[]> result = new ArrayList<String[]>();
-        permutation( people, end, start, tempResult, result, loopStartIndex );
-        for (String[] strings : result) {
-            String temp = "";
-            for (String strings2 : strings) {
-                temp += " " + strings2;
-            }
-            System.out.println(temp);
-        }
-        System.out.println(result.size());
-        
-    }
-
-    private static void permutation( String[] people, int end, int start, String[] tempResult, ArrayList<String[]> result, int loopStartIndex ) {
-        if( end == start ) {
-            result.add(tempResult.clone());
-            return;
-        }
-        
-        for( int index = loopStartIndex; index < people.length; index++ ) {
-                tempResult[start] = people[index];
-                permutation(people, end, start+1, tempResult, result, index+1);
-                tempResult[start] = "";
-        }
-    }
-
-
-    
-    private static boolean sulotions( String[][] word, String findWord, int[][] checked, int findIndex, int x, int y ) {
-        
-        int totalSize = word[0].length;
-        int[] dx = {-1,-1,-1,1,1,1,0,0};
-        int[] dy = {-1,0,1,0,-1,0,1,-1,1};
-        boolean result = false;
-        
-        if( x < 0 || x >= totalSize ) return false;
-        if( y < 0 || y >= totalSize ) return false;
-        if( checked[x][y] == 1 ) return false;
-        if( findIndex == findWord.length() ) return true;
-        
-        String findOneWord = findWord.substring( findIndex, findIndex+1 );
-        
-        if( word[x][y].equals(findOneWord) ) {
-            checked[x][y] = 1;
-            for( int index = 0; index < 8; index++ ) {
-                result = sulotions(word, findWord, checked, findIndex+1, x + dx[index], y + dy[index]);
-                if( result ) {
-                    return true;
-                }
-            }
-            checked[x][y] = 0;
-        }
-        
-        return result;
-    }
-    
-    
     
     
     public static void main(String args[]){
-        String[] people = {"1","2","3","4","5","6"};
-        result(people);
+        String[][] word = {
+                            {"N","N","N","N","S"},
+                            {"N","E","E","E","N"},
+                            {"N","E","Y","E","N"},
+                            {"N","E","E","E","N"},
+                            {"N","N","N","N","N"}
+                        };
 
-     
+        System.out.println(findWord( word, "NNNNEEENNNNNNNNNNNS" ));
+    }
+
+    private static boolean findWord(String[][] word, String findText) {
+        //1. 종료 조건 현재 위치가 마지막이고, 마지막 단어와 매칭 여부
+        //2. return 단어 존재 여부 boolean
+        //3. 한번 온 곳은 다시 가면 안됨.
+        boolean result = false;
+        boolean[][] checking = new boolean[word.length][word.length];
         
-        /*while( true ) {
-            Solution temp = new Solution();
-            Solution3 temp3 = new Solution3();
-            int n;
-            int[] lost;
-            int[] reserve;
-            
-            Map<Integer, Integer> lostMap = new HashMap<Integer, Integer>();
-            Map<Integer, Integer> lostReserve = new HashMap<Integer, Integer>();
-            
-            double random  = Math.random();
-            
-            n = ( (int)( random*100) + 1) % 30 + 1;
-            
-           
-            
-            
-            int count = 0;
-            int nextN = ( (int)( random*100) + 1) % n;
-            lost     = new int[nextN];
-            int[] lost2     = new int[nextN];
-            int[] lost3     = new int[nextN];
-            
-            while ( count < nextN ) {
-                random  = Math.random();
-                int value      = ( (int)(random*100) + 1) % n;
-                value = value == 0 ? value = 1 : value;
-                
-                if(!lostMap.containsKey(value)) {
-                    lostMap.put(value,value);
-                    lost[count] = value;
-                    count++;
-                } 
-            }
-            
-            count = 0;
-            nextN = ( (int)( random*100) + 1) % n;
-            reserve  = new int[nextN];
-            int[] reserve2     = new int[nextN];
-            int[] reserve3     = new int[nextN];
-            
-            while ( count < nextN ) {
-                random  = Math.random();
-                int value      = ( (int)(random*100) + 1) % n;
-                value = value == 0 ? value = 1 : value;
-                
-                if(!lostReserve.containsKey(value)) {
-                    lostReserve.put(value,value);0
-                    reserve[count] = value;
-                    count++;
-                } 
-            }
-            
-            Arrays.sort(lost);
-            Arrays.sort(reserve);
-            
-            for (int index = 0; index < lost.length; index++) {
-                lost2[index] = lost[index];
-                lost3[index] = lost[index];
-            }
-            for (int index = 0; index < reserve.length; index++) {
-                reserve2[index] = reserve[index];
-                reserve3[index] = reserve[index];
-            }
-            
-            int my  = temp.solution(n, lost2, reserve2);
-            int nam = temp3.solution(n, lost3, reserve3);
-            if( my != nam && n < 8) {
-                System.out.println("다르다");
-            }
-        }*/
-        
-        /*while( true ) {
-            Solution temp = new Solution();
-            Solution2 temp2 = new Solution2();
-            int n;
-            int m;
-            double random  = Math.random();
-            
-            n = ( (int)( random*100) + 1) % 10 + 2;
-            random  = Math.random();
-            m = ( (int)( random*100) + 1) % 10 + 2;
-            
-            String[] words = new String[m];
-            for (int index = 0; index < words.length; index++) {
-                StringBuilder tempaa = new StringBuilder();
-                for (int index2 = 0; index2 < n; index2++) {
-                    random  = Math.random();
-                    int tttt = ( (int)( random*100) + 1) % 26 +1;
-                    char zzz = (char) (tttt + 'A' - 1 );
-                    tempaa.append(zzz);
+        out : for (int yIndex = 0; yIndex < word.length; yIndex++) {
+            for (int xIndex = 0; xIndex < word.length; xIndex++) {
+                if( result ) {
+                    break out;
+                } else {
+                    result = checking( word, findText, 0, yIndex, xIndex , checking);
                 }
-                words[index] = tempaa.toString();
-               
             }
-            int aaaa = temp.solution(m, n, words);
-            int adasd = temp2.solution(m, n, words);
-            if( aaaa != adasd ) {
-                System.out.println("다르다");
-                break;
-            }
-        }*/
+        }
         
         
-        Solution temp = new Solution();
-        System.out.println( temp.solution(100000) );
+        return result;
+    }
+
+    private static boolean checking(String[][] word, String findText, int checkingIndex, int yIndex, int xIndex, boolean[][] checking) {
+        boolean result = false;
+        int[] yCheck = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int[] xCheck = {-1, 0, 1, -1, 1, -1, 0 ,1};
         
+        if( yIndex == word.length || yIndex < 0)  return false;
+        if( yIndex == word.length || yIndex < 0) return false;
+        if( xIndex == word.length || xIndex < 0) return false;
+        if( checking[yIndex][xIndex] ) return false;
         
-        /*Thread thred1 = new Thread() {
-            public void run(){ 
-                try { 
-                  
-                  SingleTon SingleTon1 = SingleTon.getInstace();
-                  SingleTon SingleTon2 = SingleTon.getInstace();
-                  System.out.println("두 객체가 같습니까 ? : " + (SingleTon1 == SingleTon2));
-                } catch (Exception e) { 
-                  e.printStackTrace(); 
-                } 
-              } 
-        };
+        String checkText = findText.substring(checkingIndex, checkingIndex+1);
+        String wordText = word[yIndex][xIndex];
         
-        Thread thred2 = new Thread() {
-            public void run(){ 
-                try { 
-                  SingleTon SingleTon1 = SingleTon.getInstace();
-                  SingleTon SingleTon2 = SingleTon.getInstace();
-                  System.out.println("두 객체가 같습니까 ? : " + (SingleTon1 == SingleTon2));
-                } catch (Exception e) { 
-                  e.printStackTrace(); 
-                } 
-              } 
-        };
-        thred1.start();
-        thred2.start();*/
+        if( !checkText.equals(wordText) ) {
+            return false;
+        }
         
-        /*  SingleTonMoreEffective aa = SingleTonMoreEffective.getInstace();
-        SingleTonMoreEffective bb = SingleTonMoreEffective.getInstace();
-        System.out.println(aa==bb);
+        if( findText.length()-1 == checkingIndex ) {
+           return checkText.equals(wordText);
+        }
         
-        SingleTonEnum singleTonEnum  = SingleTonEnum.getInstance();
-        System.out.println(singleTonEnum.getText());*/
+        // 8방면 검사 
+        for (int cIndex = 0; cIndex < 8; cIndex++) {
+            checking[yIndex][xIndex] = true;
+            result = checking(word, findText, checkingIndex+1, yIndex+yCheck[cIndex], xIndex+xCheck[cIndex], checking);
+            checking[yIndex][xIndex] = false;
+            if( result ) {
+                return true;
+             }
+        }
+        
+        return  result;
     }
 }
 
