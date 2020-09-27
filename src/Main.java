25184da0a387561871ccbd3982484b96dcca9a89
seokjin <7271kim@naver.com>
2020-09-27 15:14:24 +0900

    [2020.09.27] 확인중 게임판 덮기

--- src/Main.java
+++ src/Main.java
@@ -1,61 +1,107 @@
-import java.util.ArrayList;
-
 public class Main {
     
+    private static final int coverType[][][] = { { {0, 0}, {1, 0}, {0, 1} },{ {0, 0}, {0, 1}, {1, 1} },{ {0, 0}, {1, 0}, {1, 1} },{ {0, 0}, {1, 0}, {1, -1} }};
     
     public static void main(String args[]){
-        String[] name = {"석진", "우리", "현식", "희범","성우","밥"};
-        int[][] friend = {{0,1},{0,2},{1,2},{1,3},{1,4},{2,3},{2,4},{3,4},{3,5},{4,5}};
-
-        check( name, friend );
+         int[][] image = {
+                            {1,1,1,1,1,1,1,1,1,1},
+                            {1,0,0,0,0,0,0,0,0,1},
+                            {1,0,0,0,0,0,0,0,0,1},
+                            {1,0,0,0,0,0,0,0,0,1},
+                            {1,0,0,0,0,0,0,0,0,1},
+                            {1,0,0,0,0,0,0,0,0,1},
+                            {1,0,0,0,0,0,0,0,0,1},
+                            {1,1,1,1,1,1,1,1,1,1}
+                       };
+        totalCase( image );
     }
     //
 
-    private static void check(String[] name, int[][] friend) {
-        // 사람 수와 친구 쌍이 주어졌을 때, 친구끼리 쌍을 짓는 경우의 수 도출
-        // 2명을 짝을 짓고, 친구인지 확인, 친구인 경우 제거, 나머지로 반복한다. 전체를 다 돌았음에도 짝을 못짓는 경우는 불가능 하다.
-        // 친구 여부를 확인하기 위한 2차원 boolean 배열 [0][1] = true일 시 둘은 친구이다. 
-        boolean[][] isFriend = new boolean[name.length][name.length];
-        for( int[] fi : friend ) {
-            isFriend[fi[0]][fi[1]] = true;
-            isFriend[fi[1]][fi[0]] = true;
+    private static void totalCase( int[][] image ) {
+        // 4 방향 회전
+        // 종료조건 , 지금 내가 4방향을 찾아야 하는 곳이 존재하지 않을 때.
+        // return total 1개씩 더해가는 것
+        System.out.println(check( image, 0 ));
+    }
+    
+    private static int check( int[][] image, int result ) {
+        int findX = -1;
+        int findY = -1;
+        
+        out : for( int y = 0; y < image.length; y++ ) {
+            for( int x = 0; x < image[0].length; x++  ) {
+                if( image[y][x] == 0 ) {
+                    findY = y;
+                    findX = x;
+                    break out;
+                }
+            }
+        }
+        // 종료조건 : 찾을 곳이 없을 때,
+        if( findY == -1 ) return 1;
+        // 격자 덮기 시작 4방향 
+        for( int loop = 0; loop < 4; loop++ ) {
+            if( orignalRota(image, findX, findY, loop) ) {
+                int tempResult = result;
+                result += check(image ,tempResult);
+                returnRota(image, findX, findY, loop);
+                
+            }
         }
         
-        // 종료 조건 친구 쌍을 다 찾았을 때 >> 남은 친구가 없을 때 사람수가 0일때.
-        // return 친구 쌍 조합을 ArrayList
-        // 확인 여부를 위한 캐쉬
-        ArrayList<String> result = new ArrayList<>();
-        boolean[] check = new boolean[name.length];
-        StringBuilder temp = new StringBuilder();
-        compair( name.length, result, check, temp, isFriend, name);
-        System.out.println(result.size());
+        return result;
     }
-
-    private static void compair( int length, ArrayList<String> result, boolean[] check, StringBuilder temp, boolean[][] isFriend, String[] name) {
-        if( length == 0 ) {
-            result.add(temp.toString());
-            return;
+    
+    // 복구 
+    private static void returnRota( int[][] image, int x, int y, int type ) {
+        if( type == 0 ) {
+            image[y][x] = 0;
+            image[y][x+1] = 0;
+            image[y+1][x] = 0;
+        } else if( type == 1 ) {
+            image[y][x] = 0;
+            image[y+1][x+1] = 0;
+            image[y+1][x] = 0;
+        } else if( type == 2 ) {
+            image[y][x] = 0;
+            image[y][x+1] = 0;
+            image[y+1][x+1] = 0;
+        } else if( type == 3 ) {
+            image[y][x] = 0;
+            image[y+1][x] = 0;
+            image[y+1][x-1] = 0;
         }
-        int findIndex = 0;
-        for( int index = 0; index < check.length; index++ ) {
-            if( !check[index] ) {
-                findIndex = index;
-                break;
-            }
+    }
+    
+    private static boolean orignalRota( int[][] image , int x, int y, int type ) {
+     // 범위 벗어날 시 불필요
+        if( ( x+1 ) == image[0].length || ( y+1 ) == image.length || ( x -1 ) < 0 ) return false;
+        boolean result = false;
+        if( type == 0 && image[y][x] != 1 && image[y][x+1] != 1 && image[y+1][x] != 1) {
+            result = true;
+            image[y][x] = 1;
+            image[y][x+1] = 1;
+            image[y+1][x] = 1;
+        } else if(  type == 1 && image[y][x] != 1 && image[y+1][x+1] != 1 && image[y+1][x] != 1) {
+            result = true;
+            image[y][x] = 1;
+            image[y+1][x+1] = 1;
+            image[y+1][x] = 1;
+        } else if(type == 2 &&  image[y][x] != 1 && image[y][x+1] != 1 && image[y+1][x+1] != 1  ) {
+          //ㄱ 자 2차 회전
+            result = true;
+            image[y][x] = 1;
+            image[y][x+1] = 1;
+            image[y+1][x+1] = 1;
+        } else if( type == 3 && image[y][x] != 1 && image[y+1][x] != 1 && image[y+1][x-1] != 1  ) {
+          //ㄱ 자 3차 회전
+            result = true;
+            image[y][x] = 1;
+            image[y+1][x] = 1;
+            image[y+1][x-1] = 1;
         }
-        for( int second = findIndex+1; second < name.length; second++  ) {
-            if( !check[second] && isFriend[findIndex][second] ) {
-                StringBuilder copy = new StringBuilder(temp.toString());
-                check[findIndex] = true;
-                check[second] = true;
-                temp.append("( "+name[findIndex] + " ");
-                temp.append(name[second] + " )");
-                compair(length-2, result, check, temp, isFriend, name);
-                check[findIndex] = false;
-                check[second] =false;
-                temp = copy;
-            }
-        }
+        
+        return result;
     }
  }
 
