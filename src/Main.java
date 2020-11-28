f649e90f4032c28e1f1cf75867c3f9440a765e9e
seokjin <7271kim@naver.com>
2020-11-28 18:50:28 +0900

    [2020.11.28] 카드뒤집기 풀이 푸쉬

--- src/Main.java
+++ src/Main.java
@@ -3,44 +3,27 @@ public class Main {
     private static int inputIndex = 0;
     
     public static void main(String args[]){
-        quardTreeDraw("xxwwwbxwxwbbbwwxxxwwbbbwwwwbb", 16);
+        quardTreeRevers("xxwwwbxwxwbbbwwxxxwwbbbwwwwbb", 16);
         
     }
 
-    private static void quardTreeDraw( String input , int n) {
-        String[][] xy = new String[n][n];
-        decompress(0, 0, n, input, xy );
-        String result = "";
-        for( int yindex = 0; yindex < n; yindex++ ) {
-            for( int xIndex = 0; xIndex< n; xIndex++ ) {
-                result += xy[yindex][xIndex] + " ";
-            }
-            result +="\n";
-        }
+    private static void quardTreeRevers( String input , int n) {
+        String result = revers( input );
         System.out.println(result);
         
     }
 
-    private static void decompress( int x, int y, int n, String input, String[][] xy) {
+    private static String revers( String input ) {
         char inputItem = input.charAt(inputIndex++);
-        if( inputItem == 'w' ) {
-            for( int yIndex = 0; yIndex < n; yIndex++ ) {
-                for( int xIndex = 0; xIndex < n; xIndex++ ) {
-                    xy[y + yIndex][ x + xIndex] = "-";
-                }
-            }
-        } else if( inputItem == 'b' ) {
-            for( int yIndex = 0; yIndex < n; yIndex++ ) {
-                for( int xIndex = 0; xIndex < n; xIndex++ ) {
-                    xy[y + yIndex][ x + xIndex] = "+";
-                }
-            }
+        if( inputItem == 'w' || inputItem == 'b' ) {
+            return String.valueOf(inputItem);
         } else {
-            int half = n/2;
-            decompress( x, y, half, input, xy );// 좌상.
-            decompress( x+half, y, half, input, xy );// 우상.
-            decompress( x, y+half, half, input, xy );// 좌하.
-            decompress( x+half, y+half, half, input, xy );// 우하.
+            String leftUp = revers(input);
+            String rightUp = revers(input);
+            String leftDown = revers(input);
+            String rightDown = revers(input);
+            
+            return "x" + leftDown + rightDown + leftUp + rightUp;
         }
     }
  }
