f622cfa6a08dad8e6e56b0be4abfb02912b76d93
seokjin <7271kim@naver.com>
2021-01-09 19:40:25 +0900

    [2021.01.09] 프로그래머스 풀이

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,48 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Main {
     
     private static int inputIndex = 0;
     
     public static void main(String args[]){
-        quardTreeRevers("xxwwwbxwxwbbbwwxxxwwbbbwwwwbb", 16);
+        int bridge_length = 2;
+        int weight = 10;
+        int[] truck_weights = {7,4,5,6};
         
-    }
-
-    private static void quardTreeRevers( String input , int n) {
-        String result = revers( input );
-        System.out.println(result);
+        System.out.println(Solution33.solution(bridge_length, weight, truck_weights));
+        Solution33.solution(bridge_length, weight, truck_weights);
         
-    }
-
-    private static String revers( String input ) {
-        char inputItem = input.charAt(inputIndex++);
-        if( inputItem == 'w' || inputItem == 'b' ) {
-            return String.valueOf(inputItem);
-        } else {
-            String leftUp = revers(input);
-            String rightUp = revers(input);
-            String leftDown = revers(input);
-            String rightDown = revers(input);
-            
-            return "x" + leftDown + rightDown + leftUp + rightUp;
-        }
     }
  }
 
+class Solution33 {
+    public static int solution(int bridge_length, int weight, int[] truck_weights) {
+        int result = 0;
+        int sum = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        for( int truck : truck_weights ) {
+            while( true ) {
+                if( queue.isEmpty() ) {
+                    queue.add(truck);
+                    result++;
+                    sum += truck;
+                    break;
+                } else if( queue.size() == bridge_length ) {
+                    sum -= queue.poll();
+                } else {
+                    result++;
+                    if( sum + truck <= weight ) {
+                        queue.add(truck);
+                        sum += truck;
+                        break;
+                    } else {
+                        queue.add(0);
+                    }
+                }
+            }
+        }
+        
+        return result+bridge_length;
+    }
+}
